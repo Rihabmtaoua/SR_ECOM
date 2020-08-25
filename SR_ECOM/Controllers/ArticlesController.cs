405750bb354169dbd0d2f673521ae53c5e1bbf87
Rihab mtaoua <mtaouaaarihab@gmail.com>
2020-08-25 15:38:27 +0100

    Le model et ArticleControl

---
+++ SR_ECOM/Controllers/ArticlesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SR_ECOM.Models;
+
+namespace SR_ECOM.Controllers
+{
+    public class ArticlesController : ApiController
+    {
+        private DBModel db = new DBModel();
+
+        // GET: api/Articles
+        public IQueryable<Article> GetArticle()
+        {
+            return db.Article;
+        }
+
+        // GET: api/Articles/5
+        [ResponseType(typeof(Article))]
+        public IHttpActionResult GetArticle(long id)
+        {
+            Article article = db.Article.Find(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(article);
+        }
+
+        // PUT: api/Articles/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutArticle(long id, Article article)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != article.Art_Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(article).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Articles
+        [ResponseType(typeof(Article))]
+        public IHttpActionResult PostArticle(Article article)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Article.Add(article);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = article.Art_Id }, article);
+        }
+
+        // DELETE: api/Articles/5
+        [ResponseType(typeof(Article))]
+        public IHttpActionResult DeleteArticle(long id)
+        {
+            Article article = db.Article.Find(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            db.Article.Remove(article);
+            db.SaveChanges();
+
+            return Ok(article);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ArticleExists(long id)
+        {
+            return db.Article.Count(e => e.Art_Id == id) > 0;
+        }
+    }
+}
