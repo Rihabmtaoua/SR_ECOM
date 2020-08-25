c3338a1d363edfbf261751054e58e41524e567bc
Rihab mtaoua <mtaouaaarihab@gmail.com>
2020-08-25 15:40:31 +0100

    UserControler

---
+++ SR_ECOM/Controllers/UtilisateursController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SR_ECOM.Models;
+
+namespace SR_ECOM.Controllers
+{
+    public class UtilisateursController : ApiController
+    {
+        private DBModel db = new DBModel();
+
+        // GET: api/Utilisateurs
+        public IQueryable<Utilisateur> GetUtilisateur()
+        {
+            return db.Utilisateur;
+        }
+
+        // GET: api/Utilisateurs/5
+        [ResponseType(typeof(Utilisateur))]
+        public IHttpActionResult GetUtilisateur(int id)
+        {
+            Utilisateur utilisateur = db.Utilisateur.Find(id);
+            if (utilisateur == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(utilisateur);
+        }
+
+        // PUT: api/Utilisateurs/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUtilisateur(int id, Utilisateur utilisateur)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != utilisateur.Utili_Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(utilisateur).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UtilisateurExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Utilisateurs
+        [ResponseType(typeof(Utilisateur))]
+        public IHttpActionResult PostUtilisateur(Utilisateur utilisateur)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Utilisateur.Add(utilisateur);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = utilisateur.Utili_Id }, utilisateur);
+        }
+
+        // DELETE: api/Utilisateurs/5
+        [ResponseType(typeof(Utilisateur))]
+        public IHttpActionResult DeleteUtilisateur(int id)
+        {
+            Utilisateur utilisateur = db.Utilisateur.Find(id);
+            if (utilisateur == null)
+            {
+                return NotFound();
+            }
+
+            db.Utilisateur.Remove(utilisateur);
+            db.SaveChanges();
+
+            return Ok(utilisateur);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UtilisateurExists(int id)
+        {
+            return db.Utilisateur.Count(e => e.Utili_Id == id) > 0;
+        }
+    }
+}
